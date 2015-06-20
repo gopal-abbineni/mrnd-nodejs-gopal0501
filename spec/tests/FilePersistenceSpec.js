a584d2c8f1699a05928a1fc9d78ec22a7d9c9a07
gopalabbineni <abbineninaga501@gmail.com>
2015-06-20 23:41:04 +0700

    commit changes

---
+++ spec/tests/FilePersistenceSpec.js
@@ -0,0 +1,124 @@
+
+var fs=require('fs');
+var getContactFileName = function(id) {
+
+	// We assume contacts are stored under data sub-folder
+	return "..\\..\\src\\contacts\\data\\" + id + "-Contact.json";
+}
+
+var request = require('C:/Program Files/nodejs/node_modules/npm/node_modules/request');
+var idCreated;
+
+describe("FilePersistence Test Suite", function(){
+
+	//var request = require('request');
+
+	var base_url = "http://localhost:3000";
+	var contacts_url = base_url + "/contacts";
+	//var fs = require('fs');
+
+	describe("create persist contact", function(){
+		
+
+		it("should create contact",function(done){
+
+			var contact = new Object();
+			contact.firstName = "jagan";
+			contact.lastName = "peri";
+			contact.phone = "23002300";
+
+			console.log(JSON.stringify(contact));
+		    
+		    request.post({url: contacts_url,
+		    			  body: contact,
+		    			  json: true
+		    			}, 
+		    		    function(error, response, body){
+                
+							expect(response.statusCode).toBe(200);
+							console.log(body);
+							idCreated = body;
+							done();
+					    });
+		});
+
+		xit("should persist contact",function(done){
+
+			var fileName = getContactFileName(idCreated);
+
+			var obj = JSON.parse(fs.readFileSync(fileName));
+
+			expect(obj.firstName).toBe("jagan");
+			done();
+
+		});
+		xit("should update contact",function(done){
+
+			var updatedContact = new Object();
+			updatedContact.firstName = "jagan-updated";
+			request.put({
+							url: contacts_url + "/" + idCreated,
+							body: updatedContact,
+							json: true
+						},
+		    		    function(error, response, body){
+
+							expect(response.statusCode).toBe(200);
+							console.log(body);
+
+							var fileName = getContactFileName(idCreated);
+
+							var obj = JSON.parse(fs.readFileSync(fileName));
+							expect(obj.firstName).toBe("jagan-updated");
+							done();
+					    });
+		});
+	});
+
+	//TODO: Fill out the test case below that posts a message to a contact
+	// and retrieves it back.
+	describe("post and get message to contact", function(){
+		xit("should post message to contact", function(done){
+			//TODO: Write your test case here.
+           var updatedContact = new Object();
+			updatedContact.message = "jagan-message";
+			request.put({
+							url: contacts_url + "/" + idCreated,
+							body: updatedContact,
+							json: true
+						},
+		    		    function(error, response, body){
+
+							expect(response.statusCode).toBe(200);
+							console.log(body);
+                
+							var fileName = getContactFileName(idCreated);
+                var obj = JSON.parse(fs.readFileSync(fileName));
+				console.log(JSON.stringify(obj.message));	
+                expect(obj.message).toBe("jagan-message");
+							done();
+					    });
+        });
+
+		xit("should get message for contact", function(done){
+			//TODO: Write your test case here.
+            request.get({
+							url: contacts_url + "/" + idCreated,
+							json: true
+						},
+		    		    function(error, response, body){
+                console.log("satya");
+							expect(response.statusCode).toBe(200);
+							console.log(body);
+                var fileName = getContactFileName(idCreated);
+var obj = JSON.parse(fs.readFileSync(fileName));
+							expect(obj.message).toBe("jagan-message");
+                done();
+					    });
+			
+
+		});
+
+	});
+
+});
--- src/contacts/routes/contacts.js
+++ src/contacts/routes/contacts.js
@@ -1,28 +1,61 @@
 var express = require('express');
 var arr=new Array(),index=0;
 var router = express.Router();
+var fs = require('fs');
+
+var getContactFileName = function(id) {
+
+	// We assume contacts are stored under data sub-folder
+	return "data\\" +id+ "-Contact.json";
+}
 
 /* GET contacts */
+router.get('/', function(req, res, next) {
+	res.send(arr);
+});
 router.get('/:id', function(req, res, next) {
-	res.send(arr[+req.params.id]);
+    var filename=getContactFileName(+req.params.id);
+    fs.readFile(filename, function (err, data) {
+  var a2=JSON.parse(data);
+        console.log(a2);
+  res.send(a2);
+});
 });
 
 router.post('/', function(req, res, next) {
-	arr[index++]=req.body;
+    var filename=getContactFileName(index);
+    console.log(filename);
+    arr[index++]=req.body;
+    fs.writeFile(filename,JSON.stringify(arr[index-1]),function(err){
+        if(err) throw err;
+        console.log('saved');
+    });
 	res.send(""+(index-1));
-  console.log(req.body);
+  //console.log(req.body);
 });
 
 router.put('/:id', function(req, res, next) {
-  console.log(req.body);
-  var a1=req.body;
-  var a2=arr[+req.params.id];
+ // console.log(req.body);
+  var filename=getContactFileName(+req.params.id);
+    fs.readFile(filename, function (err, data) {
+  if (err) throw err;
+         var a1=req.body;
+  var a2=JSON.parse(data);
   for(var i in a1)
   {
 	  a2[i]=a1[i];
   }
   arr[+req.params.id]=a2;
+  console.log(JSON.stringify(data));
+          fs.writeFile(filename,JSON.stringify(arr[+req.params.id]),function(err){
+        if(err) throw err;
+        console.log('saved');
+    });  
   res.send(a2);
+});
+    
+ 
+    
 });
 
 module.exports = router;
